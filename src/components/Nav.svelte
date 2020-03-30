1eece5ec5c73cb95b81db58af5842c0b0d5bfb9a
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-03-31 00:12:09 +0200

    handle countries/{country} path to navigation. Remove sample blog items

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -52,9 +52,10 @@
 	<ul>
 		<li><a aria-current='{segment === undefined ? "page" : undefined}' href='.'>home</a></li>
 		<li><a aria-current='{segment === "about" ? "page" : undefined}' href='about'>about</a></li>
+		{#if segment === "countries" }
+		<li><a aria-current='{segment === "countries" ? "page" : undefined}' href='/'> {'< '}
+		</a></li>
+		{/if}
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current='{segment === "blog" ? "page" : undefined}' href='blog'>blog</a></li>
 	</ul>
 </nav>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,4 +7,5 @@
 
 <h1>COVID 19 Stats</h1>
 <h2>app.name - app.version</h2>
-<p>app.description</p>
+<p>app.description. 
+All the information is fetched from <a href="http://covid-rest.appspot.com/docs/index.html">REST API</a> exposing data from  <a href="https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide">ECDC</a>.</p>
