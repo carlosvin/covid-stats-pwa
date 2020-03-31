7b5fa25fecafe88f42b79a13b10ba3af6a0661b3
Carlos Martin Sanchez <carlsa@microsoft.com>
2020-03-31 11:36:08 +0200

    styling about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,14 @@
 <script>
 </script>
+
+<style>
+h2 {
+	font-family: 'Courier New', Courier, monospace;
+	color: gray;
+	font-size: smaller;
+}
+</style>
+
 <svelte:head>
 	<title>About</title>
 	<meta name="description" content="app.description">
@@ -7,5 +16,5 @@
 
 <h1>COVID 19 Stats</h1>
 <h2>app.name - app.version</h2>
-<p>app.description. 
-All the information is fetched from <a href="http://covid-rest.appspot.com/docs/index.html">REST API</a> exposing data from  <a href="https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide">ECDC</a>.</p>
+<p>app.description.</p>
+<p>All the information is fetched from <a href="http://covid-rest.appspot.com/docs/index.html">REST API</a> exposing data from  <a href="https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide">ECDC</a>.</p>
