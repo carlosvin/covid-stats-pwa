a22043fa6ca475f14f993f0bc9d2b2631d1403e2
Carlos Martin Sanchez <carlsa@microsoft.com>
2020-03-31 11:49:54 +0200

    add link to github project

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,6 +7,13 @@ h2 {
 	color: gray;
 	font-size: smaller;
 }
+/* TODO make footer reusable as we do with Nav */
+footer {
+	color: gray;
+	margin-bottom: 0;
+  	width: 100%;
+  	height: 2.5em;
+}
 </style>
 
 <svelte:head>
@@ -18,3 +25,6 @@ h2 {
 <h2>app.name - app.version</h2>
 <p>app.description.</p>
 <p>All the information is fetched from <a href="http://covid-rest.appspot.com/docs/index.html">REST API</a> exposing data from  <a href="https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide">ECDC</a>.</p>
+<footer>
+<p>This is Open Source project, you can find the source code at: <a href="https://github.com/carlosvin/covid-stats-pwa">https://github.com/carlosvin/covid-stats-pwa</a></p>
+</footer>
