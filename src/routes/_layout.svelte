d0efc6db7e67e214adb54c1cfa46e70d57032633
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-04-01 18:12:29 +0200

    add meta tags to improve link previews

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,43 +1,61 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
-	import {keywords} from '../../package.json';
+  import Nav from "../components/Nav.svelte";
+  import Footer from "../components/Footer.svelte";
+  import { keywords, description, homepage } from "../../package.json";
+  import { name } from "../../static/manifest.json";
 
-	export let segment;
+  export let segment;
 
-	const date = new Date();
+  const date = new Date();
 </script>
 
-<svelte:head>
-	<meta name="keywords" content="{keywords}"/>
-	<meta name="robots" content="index,follow" />
-	<meta name="revised" content="{date.toISOString()}" />
-</svelte:head>
-
 <style>
-	main {
-		display: flex;
-		flex-wrap: wrap;
-		flex-flow: column;
-		justify-content: start;
-		justify-items: flex-start;
-		
-		width: 100%;
-		background-color: white;
-		padding: 2rem;
-		margin-top: 0;
-		margin-left: 0;
-		margin-right: 0;
-		margin-bottom: 1em;
-		box-sizing: border-box;
-		text-align: center;
-	}
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    flex-flow: column;
+    justify-content: start;
+    justify-items: flex-start;
+
+    width: 100%;
+    background-color: white;
+    padding: 2rem;
+    margin-top: 0;
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 1em;
+    box-sizing: border-box;
+    text-align: center;
+  }
 </style>
 
-<Nav {segment}/>
+<svelte:head>
+  <meta name="title" content={name} />
+  <meta property="og:title" content={name} />
+  <meta property="twitter:title" content={name} />
+
+  <meta name="description" content={description} />
+  <meta property="og:description" content={description} />
+  <meta property="twitter:description" content={description} />
+
+  <meta property="og:url" content={homepage} />
+  <meta property="twitter:url" content={homepage} />
+
+  <meta property="og:image" content="/logo-192.png" />
+  <meta property="twitter:image" content="/logo-192.png" />
+
+  <meta property="og:type" content="website" />
+  <meta property="twitter:card" content="summary_large_image" />
+
+  <meta name="keywords" content={keywords} />
+  <meta name="robots" content="index,follow" />
+  <meta name="revised" content={date.toISOString()} />
+</svelte:head>
+
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
 
-<Footer/>
+<Footer />
