4f918cb8b6a2b9b3a2d8fee154df765779bafddf
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-04-02 16:04:09 +0200

    Close #5: show offline

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,15 @@
 <script>
 	export let segment;
+	let isOffline = false;
+
+	if (typeof window !== 'undefined') {
+		window.addEventListener('online',  updateOnlineStatus);
+		window.addEventListener('offline', updateOnlineStatus);	
+	}
+
+	function updateOnlineStatus(event) {
+		isOffline = navigator.onLine ? false : true;
+	}
 </script>
 
 <style>
@@ -7,11 +17,24 @@
 		border-bottom: 1px solid rgba(221,94,75,0.1);
 		font-weight: 300;
 		padding: 0 1em;
+		display: flex;
+		flex-flow: row;
+		vertical-align: middle;
+	}
+
+	.offline {
+		color: brown;
+		border-radius: 0.3em;
+		padding: 0.3em;
+		vertical-align: middle;
+		border: 1px solid;
+		font-size: smaller;
 	}
 
 	ul {
 		margin: 0;
 		padding: 0;
+		flex-grow: 1;
 	}
 
 	/* clearfix */
@@ -56,6 +79,8 @@
 		<li><a aria-current='{segment === "countries" ? "page" : undefined}' href='/'> {'< '}
 		</a></li>
 		{/if}
-
 	</ul>
+	{#if isOffline}
+		<p class='offline'>offline</p>
+	{/if}
 </nav>
